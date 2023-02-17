1c13b80b828415a6f76f3f78b2d2be50b4eca213
Richard Hua <rich@rkhua.com>
2023-02-17 15:15:40 -0800

    refactor: load all messages on startup

--- src/message-backup/TopicStoreBackupClient.ts
+++ src/message-backup/TopicStoreBackupClient.ts
@@ -7,9 +7,9 @@ import BackupClient, {
 const BACKUP_TYPE = BackupType.xmtpTopicStore
 export default class TopicStoreBackupClient implements BackupClient {
   private _configuration: TopicStoreBackupConfiguration
+  private _conversations: Conversations
   // A queue of background tasks to be done. Each promise is chained onto the previous one.
   private _queue: Promise<void>
-  private _conversations: Conversations
 
   public static createConfiguration(
     walletAddress: string
@@ -28,16 +28,44 @@ export default class TopicStoreBackupClient implements BackupClient {
   ) {
     this._configuration = configuration
     this._conversations = conversations
-    this._queue = this.initialize()
+    this._queue = Promise.resolve()
+    this.initialize()
   }
 
   public get backupType(): BackupType {
     return BACKUP_TYPE
   }
 
-  private async initialize(): Promise<void> {
-    // 1. fetch everything
-    // 2. incremental updates after that
-    return Promise.resolve()
+  private initialize(): void {
+    // TODO: error handling
+    this.queueTask(async () => {
+      const conversations = await this._conversations.list()
+      await Promise.all(
+        conversations.map(async (conversation) => {
+          const messagesInConversation = await conversation.messages()
+          console.log(messagesInConversation)
+        })
+      )
+    })
+    this.subscribeToNewMessages()
+  }
+
+  private async subscribeToNewMessages(): Promise<void> {
+    for await (const message of await this._conversations.streamAllMessages()) {
+      // if (message.senderAddress === this.configuration_address) {
+      //   // This message was sent from me
+      //   continue
+      // }
+      this.queueTask(() => {
+        console.log(
+          `New message from ${message.senderAddress}: ${message.content}`
+        )
+        return Promise.resolve()
+      })
+    }
+  }
+
+  private queueTask(task: () => Promise<void>): void {
+    this._queue = this._queue.then(task)
   }
 }
